b120e9fc5927072cb28b078783197bf0eda1e939
SAUL_ALEXANDER_JIMENEZ_RUEDA <a.22308051281222@cbtis128.edu.mx>
2025-02-27 19:42:09 +0000

    jimenez container

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiContainer());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiContainer extends StatelessWidget {
+  const MiContainer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('      saul container \n Mat: 22308051281222'),
+          centerTitle: true,
+          backgroundColor: const Color(0xff132bb4),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Primer contenedor
+              Container(
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color(0xffd54c42), // Color del contenedor
+                  borderRadius:
+                      BorderRadius.circular(15), // Esquinas redondeadas
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26
+                          .withOpacity(0.20), // Color de la sombra
+                      spreadRadius: 5, // Extensión de la sombra
+                      blurRadius: 7, // Difuminado de la sombra
+                      offset: const Offset(0, 3), // Desplazamiento de la sombra
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Contenedor 1',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // Espacio entre los contenedores
+              // Segundo contenedor
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+  } // fin de widgets
+} // clase mi contenendor
